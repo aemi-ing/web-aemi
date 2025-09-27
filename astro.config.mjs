bb63141f9b99e2cf1fbc65e83c12468ca67b0475
Javier Gamboa <jgamboa19@alumnos.utalca.cl>
2025-09-26 21:52:21 -0300

    intento numero 1012030123

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   site: 'https://aemiingenieria.cl',
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date()
-    })
-  ]
 });
