bb18ef87436131cec72fe182786597a38d055e7d
Javier Gamboa <jgamboa19@alumnos.utalca.cl>
2025-09-26 21:47:38 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,24 +9,9 @@ export default defineConfig({
   site: 'https://aemiingenieria.cl',
   integrations: [
     sitemap({
-      customPages: [
-        'https://aemiingenieria.cl/',
-        'https://aemiingenieria.cl/#services',
-        'https://aemiingenieria.cl/#gallery', 
-        'https://aemiingenieria.cl/#about',
-        'https://aemiingenieria.cl/#contact',
-        'https://aemiingenieria.cl/#ofertas'
-      ],
-      serialize(item) {
-        if (item.url === 'https://aemiingenieria.cl/') {
-          item.priority = 1.0;
-          item.changefreq = 'weekly';
-        } else {
-          item.priority = 0.7;
-          item.changefreq = 'monthly';
-        }
-        return item;
-      }
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
     })
   ]
 });
