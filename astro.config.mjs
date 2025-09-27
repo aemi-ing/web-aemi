2964b0c8634f5e5d16c3dcec9bb4d8b143741405
Javier Gamboa <jgamboa19@alumnos.utalca.cl>
2025-09-26 21:25:39 -0300

    mjs para sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      headers: {
+        '/sitemap.xml': {
+          'Content-Type': 'application/xml; charset=utf-8'
+        }
+      }
+    }
   },
   site: 'https://aemiingenieria.cl',
 });
